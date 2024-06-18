de35c6afb16e0c2587486b0a12df057429625043
Oscar Maciel Castillo <108559153+OscarMacielC@users.noreply.github.com>
2024-06-18 02:42:07 -0600

    Bullets hit objects and destroy themselves

--- Assets/Scripts/Enemy/Shoot.cs
+++ Assets/Scripts/Enemy/Shoot.cs
@@ -1,12 +1,28 @@
+using System;
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Shoot : MonoBehaviour
 {
     [SerializeField] private float speed = 1f;
+    [SerializeField] private float lifespan = 5f;
     private void Update()
     {
         transform.Translate(Vector3.forward *Time.deltaTime*speed);
+        if (isActiveAndEnabled)
+        {
+            StartCoroutine(disableShot());
+        }
+    }
+
+    IEnumerator disableShot()
+    {
+        yield return new WaitForSeconds(lifespan);
+        gameObject.SetActive(false);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        gameObject.SetActive(false);
     }
 }
